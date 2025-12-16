9eb9c4355bdbf25c83fa422ba348ad45420a773a
emiryasa <emirys774@gmail.com>
2025-12-16 14:06:45 +0300

    feat: prisma config error fix

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,8 +1,18 @@
 import { INestApplication, Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Client } from 'pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    const connectionString = process.env.DATABASE_URL;
+    const client = new Client({ connectionString });
+    const adapter = new PrismaPg(client);
+
+    super({ adapter });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
